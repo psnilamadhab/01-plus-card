0691ace5bb5f96df934768f2fc58407908af92df
nayakdebananda <rdebanandanayak@gmail.com>
2025-03-09 20:27:42 +0530

    bug:remove pnpm engine

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  title: 'Plus-card',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
